2bc374a245889849bdf42b3d4c0b36ce1c80c4d3
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-10-22 16:39:01 -0500

    9. Exercise 1

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/GetReputationEndpoint.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/GetReputationEndpoint.kt
@@ -4,10 +4,10 @@ import com.techyourchance.coroutines.common.ThreadInfoLogger
 import kotlin.random.Random
 
 class GetReputationEndpoint {
-    fun getReputation(userId: String): Int {
-        ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): called")
-        Thread.sleep(3000)
-        ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation(): return data")
-        return Random.nextInt(0, 100)
-    }
+	suspend fun getReputation(userId: String): Int {
+		ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation($userId): called")
+		Thread.sleep(3000)
+		ThreadInfoLogger.logThreadInfo("GetReputationEndpoint#getReputation($userId): return data")
+		return Random.nextInt(0, 100)
+	}
 }
