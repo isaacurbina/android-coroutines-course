5b9fe8aa64f6ecc1076890fa27734966db570a28
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-10-22 16:39:31 -0500

    9. Exercise 1 (#4)

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/Exercise1Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise1/Exercise1Fragment.kt
@@ -1,11 +1,11 @@
 package com.techyourchance.coroutines.exercises.exercise1
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -14,61 +14,75 @@ import com.techyourchance.coroutines.R
 import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class Exercise1Fragment : BaseFragment() {
 
-    override val screenTitle get() = ScreenReachableFromHome.EXERCISE_1.description
+	private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
 
-    private lateinit var edtUserId: EditText
-    private lateinit var btnGetReputation: Button
+	override val screenTitle get() = ScreenReachableFromHome.EXERCISE_1.description
 
-    private lateinit var getReputationEndpoint: GetReputationEndpoint
+	private lateinit var edtUserId: EditText
+	private lateinit var btnGetReputation: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        getReputationEndpoint = compositionRoot.getReputationEndpoint
-    }
+	private lateinit var getReputationEndpoint: GetReputationEndpoint
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_exercise_1, container, false)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		getReputationEndpoint = compositionRoot.getReputationEndpoint
+	}
 
-        edtUserId = view.findViewById(R.id.edt_user_id)
-        edtUserId.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {            }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		logThreadInfo("onCreateView()")
+		val view = inflater.inflate(R.layout.fragment_exercise_1, container, false)
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                btnGetReputation.isEnabled = !s.isNullOrEmpty()
-            }
+		edtUserId = view.findViewById(R.id.edt_user_id)
+		edtUserId.addTextChangedListener(object : TextWatcher {
+			override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-            override fun afterTextChanged(s: Editable?) {}
-        })
+			override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+				btnGetReputation.isEnabled = !s.isNullOrEmpty()
+			}
 
-        btnGetReputation = view.findViewById(R.id.btn_get_reputation)
-        btnGetReputation.setOnClickListener {
-            logThreadInfo("button callback")
-            btnGetReputation.isEnabled = false
-            getReputationForUser(edtUserId.text.toString())
-            btnGetReputation.isEnabled = true
-        }
+			override fun afterTextChanged(s: Editable?) {}
+		})
 
-        return view
-    }
+		btnGetReputation = view.findViewById(R.id.btn_get_reputation)
+		btnGetReputation.setOnClickListener {
+			logThreadInfo("button callback")
+			btnGetReputation.isEnabled = false
+			coroutineScope.launch {
+				val reputation = getReputationForUser(edtUserId.text.toString())
+				Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT)
+					.show()
+				btnGetReputation.isEnabled = true
+			}
+		}
 
-    private fun getReputationForUser(userId: String) {
-        logThreadInfo("getReputationForUser()")
+		return view
+	}
 
-        val reputation = getReputationEndpoint.getReputation(userId)
+	private suspend fun getReputationForUser(userId: String): Int {
+		logThreadInfo("getReputationForUser()")
+		return withContext(Dispatchers.IO) {
+			getReputationEndpoint.getReputation(userId)
+		}
+	}
 
-        Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
-    }
+	private fun logThreadInfo(message: String) {
+		ThreadInfoLogger.logThreadInfo(message)
+	}
 
-    private fun logThreadInfo(message: String) {
-        ThreadInfoLogger.logThreadInfo(message)
-    }
-
-    companion object {
-        fun newInstance(): Fragment {
-            return Exercise1Fragment()
-        }
-    }
+	companion object {
+		fun newInstance(): Fragment {
+			return Exercise1Fragment()
+		}
+	}
 }
