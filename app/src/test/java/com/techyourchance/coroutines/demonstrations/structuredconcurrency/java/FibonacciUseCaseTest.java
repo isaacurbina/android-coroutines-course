df9b004e15648f404d1181fea723fcf9e2c125b4
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-10-29 09:27:15 -0500

    26. Fibonacci Computation

--- app/src/test/java/com/techyourchance/coroutines/demonstrations/structuredconcurrency/java/FibonacciUseCaseTest.java
+++ app/src/test/java/com/techyourchance/coroutines/demonstrations/structuredconcurrency/java/FibonacciUseCaseTest.java
@@ -1,56 +1,64 @@
 package com.techyourchance.coroutines.demonstrations.structuredconcurrency.java;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigInteger;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
-
 public class FibonacciUseCaseTest {
 
-    FibonacciUseCase SUT;
+	FibonacciUseCase SUT;
 
-    @Before
-    public void setup() throws Exception {
-        SUT = new FibonacciUseCase();
-    }
+	@Before
+	public void setup() {
+		SUT = new FibonacciUseCase();
+	}
 
-    @Test
-    public void computeFibonacci_0_returns0() throws Exception {
-        // Arrange
-        // Act
-        BigInteger result = SUT.computeFibonacci(0);
-        // Assert
-        assertThat(result, is(new BigInteger("0")));
-    }
+	@Test
+	public void computeFibonacci_0_returns0() {
+		// Arrange
+		// Act
+		BigInteger result = SUT.computeFibonacci(0);
+		// Assert
+		assertThat(result, is(new BigInteger("0")));
+	}
 
-    @Test
-    public void computeFibonacci_1_returns1() throws Exception {
-        // Arrange
-        // Act
-        BigInteger result = SUT.computeFibonacci(1);
-        // Assert
-        assertThat(result, is(new BigInteger("1")));
-    }
+	@Test
+	public void computeFibonacci_1_returns1() {
+		// Arrange
+		// Act
+		BigInteger result = SUT.computeFibonacci(1);
+		// Assert
+		assertThat(result, is(new BigInteger("1")));
+	}
 
-    @Test
-    public void computeFibonacci_10_returnsCorrectAnswer() throws Exception {
-        // Arrange
-        // Act
-        BigInteger result = SUT.computeFibonacci(10);
-        // Assert
-        assertThat(result, is(new BigInteger("55")));
-    }
+	@Test
+	public void computeFibonacci_10_returnsCorrectAnswer() {
+		// Arrange
+		// Act
+		BigInteger result = SUT.computeFibonacci(10);
+		// Assert
+		assertThat(result, is(new BigInteger("55")));
+	}
 
-    @Test
-    public void computeFibonacci_30_returnsCorrectAnswer() throws Exception {
-        // Arrange
-        // Act
-        BigInteger result = SUT.computeFibonacci(30);
-        // Assert
-        assertThat(result, is(new BigInteger("832040")));
-    }
+	@Test
+	public void computeFibonacci_30_returnsCorrectAnswer() {
+		// Arrange
+		// Act
+		BigInteger result = SUT.computeFibonacci(30);
+		// Assert
+		assertThat(result, is(new BigInteger("832040")));
+	}
 
+	@Test
+	public void computeFibonacci_31_returnsCorrectAnswer() {
+		// Arrange
+		// Act
+		BigInteger result = SUT.computeFibonacci(31);
+		// Assert
+		assertThat(result, is(new BigInteger("1346269")));
+	}
 }
