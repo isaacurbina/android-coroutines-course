73df527f1772422f6f1c18486313637f8e1c8f6e
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-10-29 09:58:03 -0500

    28. Concurrent Fibonacci Computation with Callback on UI Thread

--- app/src/test/java/com/techyourchance/coroutines/demonstrations/structuredconcurrency/java/FibonacciUseCaseAsyncUiTest.java
+++ app/src/test/java/com/techyourchance/coroutines/demonstrations/structuredconcurrency/java/FibonacciUseCaseAsyncUiTest.java
@@ -1,69 +1,63 @@
 package com.techyourchance.coroutines.demonstrations.structuredconcurrency.java;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigInteger;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.*;
-
 public class FibonacciUseCaseAsyncUiTest {
 
-    FibonacciUseCaseAsyncUi.Callback mCallback;
-    FibonacciUseCaseAsyncUi SUT;
+	FibonacciUseCaseAsyncUi.Callback mCallback;
+	FibonacciUseCaseAsyncUi SUT;
 
-    BigInteger lastResult = null;
+	BigInteger lastResult = null;
 
-    @Before
-    public void setup() throws Exception {
-        mCallback = new FibonacciUseCaseAsyncUi.Callback() {
-            @Override
-            public void onFibonacciComputed(BigInteger result) {
-                lastResult = result;
-            }
-        };
-        SUT = new FibonacciUseCaseAsyncUi();
-    }
+	@Before
+	public void setup() {
+		mCallback = result -> lastResult = result;
+		SUT = new FibonacciUseCaseAsyncUi();
+	}
 
-    @Test
-    public void computeFibonacci_0_returns0() throws Exception {
-        // Arrange
-        // Act
-        SUT.computeFibonacci(0, mCallback);
-        Thread.sleep(10);
-        // Assert
-        assertThat(lastResult, is(new BigInteger("0")));
-    }
+	@Test
+	public void computeFibonacci_0_returns0() throws Exception {
+		// Arrange
+		// Act
+		SUT.computeFibonacci(0, mCallback);
+		Thread.sleep(10);
+		// Assert
+		assertThat(lastResult, is(new BigInteger("0")));
+	}
 
-    @Test
-    public void computeFibonacci_1_returns1() throws Exception {
-        // Arrange
-        // Act
-        SUT.computeFibonacci(1, mCallback);
-        Thread.sleep(10);
-        // Assert
-        assertThat(lastResult, is(new BigInteger("1")));
-    }
+	@Test
+	public void computeFibonacci_1_returns1() throws Exception {
+		// Arrange
+		// Act
+		SUT.computeFibonacci(1, mCallback);
+		Thread.sleep(10);
+		// Assert
+		assertThat(lastResult, is(new BigInteger("1")));
+	}
 
-    @Test
-    public void computeFibonacci_10_returnsCorrectAnswer() throws Exception {
-        // Arrange
-        // Act
-        SUT.computeFibonacci(10, mCallback);
-        Thread.sleep(200);
-        // Assert
-        assertThat(lastResult, is(new BigInteger("55")));
-    }
+	@Test
+	public void computeFibonacci_10_returnsCorrectAnswer() throws Exception {
+		// Arrange
+		// Act
+		SUT.computeFibonacci(10, mCallback);
+		Thread.sleep(200);
+		// Assert
+		assertThat(lastResult, is(new BigInteger("55")));
+	}
 
-    @Test
-    public void computeFibonacci_30_returnsCorrectAnswer() throws Exception {
-        // Arrange
-        // Act
-        SUT.computeFibonacci(30, mCallback);
-        Thread.sleep(200);
-        // Assert
-        assertThat(lastResult, is(new BigInteger("832040")));
-    }
-
+	@Test
+	public void computeFibonacci_30_returnsCorrectAnswer() throws Exception {
+		// Arrange
+		// Act
+		SUT.computeFibonacci(30, mCallback);
+		Thread.sleep(200);
+		// Assert
+		assertThat(lastResult, is(new BigInteger("832040")));
+	}
 }
