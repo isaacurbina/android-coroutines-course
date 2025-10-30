0da10d0170c032b7e19604a8a75a81044960c2b7
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-10-30 11:13:07 -0500

    31. Concurrent Fibonacci Computation Using Coroutines (Callback)

--- app/src/main/java/com/techyourchance/coroutines/demonstrations/structuredconcurrency/kotlin/FibonacciUseCaseAsyncUiCoroutines.kt
+++ app/src/main/java/com/techyourchance/coroutines/demonstrations/structuredconcurrency/kotlin/FibonacciUseCaseAsyncUiCoroutines.kt
@@ -1,31 +1,32 @@
 package com.techyourchance.coroutines.demonstrations.structuredconcurrency.kotlin
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.math.BigInteger
 
 internal class FibonacciUseCaseAsyncUiCoroutines(private val bgDispatcher: CoroutineDispatcher) {
 
-    interface Callback {
-        fun onFibonacciComputed(result: BigInteger?)
-    }
+	interface Callback {
+		fun onFibonacciComputed(result: BigInteger?)
+	}
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+	private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
-    fun computeFibonacci(index: Int, callback: Callback) {
-        coroutineScope.launch {
-            val result = computeFibonacciBg(index)
-            callback.onFibonacciComputed(result)
-        }
-    }
+	fun computeFibonacci(index: Int, callback: Callback) {
+		coroutineScope.launch {
+			val result = computeFibonacciBg(index)
+			callback.onFibonacciComputed(result)
+		}
+	}
 
-    private suspend fun computeFibonacciBg(index: Int): BigInteger = withContext(bgDispatcher) {
-        if (index == 0) {
-            BigInteger("0")
-        } else if (index == 1) {
-            BigInteger("1")
-        } else {
-            computeFibonacciBg(index - 1).add(computeFibonacciBg(index - 2))
-        }
-    }
-
+	private suspend fun computeFibonacciBg(index: Int): BigInteger = withContext(bgDispatcher) {
+		when (index) {
+			0 -> BigInteger("0")
+			1 -> BigInteger("1")
+			else -> computeFibonacciBg(index - 1).add(computeFibonacciBg(index - 2))
+		}
+	}
 }
