4c685603fb0b60e86f26949dffb2f930ea26aad6
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-05 14:41:25 -0600

    37. Encapsulating Concurrency in Use Cases

--- app/src/main/java/com/techyourchance/coroutines/demonstrations/design/DesignDemoFragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/demonstrations/design/DesignDemoFragment.kt
@@ -12,85 +12,93 @@ import com.techyourchance.coroutines.R
 import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class DesignDemoFragment : BaseFragment() {
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
+	private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
 
-    override val screenTitle get() = ScreenReachableFromHome.DESIGN_DEMO.description
+	override val screenTitle get() = ScreenReachableFromHome.DESIGN_DEMO.description
 
-    private lateinit var benchmarkUseCase: BenchmarkUseCase
+	private lateinit var benchmarkUseCase: BenchmarkUseCase
 
-    private lateinit var btnStart: Button
-    private lateinit var txtRemainingTime: TextView
+	private lateinit var btnStart: Button
+	private lateinit var txtRemainingTime: TextView
 
-    private var hasBenchmarkBeenStartedOnce = false
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        benchmarkUseCase = compositionRoot.benchmarkUseCase
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_loop_iterations_demo, container, false)
-
-        txtRemainingTime = view.findViewById(R.id.txt_remaining_time)
-
-        btnStart = view.findViewById(R.id.btn_start)
-        btnStart.setOnClickListener {
-            logThreadInfo("button callback")
-
-            val benchmarkDurationSeconds = 5
-
-            coroutineScope.launch {
-                updateRemainingTime(benchmarkDurationSeconds)
-            }
-
-            coroutineScope.launch {
-                btnStart.isEnabled = false
-                val iterationsCount = benchmarkUseCase.executeBenchmark(benchmarkDurationSeconds)
-                Toast.makeText(requireContext(), "$iterationsCount", Toast.LENGTH_SHORT).show()
-                btnStart.isEnabled = true
-            }
-
-            hasBenchmarkBeenStartedOnce = true
-        }
-
-        return view
-    }
-
-    override fun onStop() {
-        logThreadInfo("onStop()")
-        super.onStop()
-        coroutineScope.coroutineContext.cancelChildren()
-        if (hasBenchmarkBeenStartedOnce) {
-            btnStart.isEnabled = true
-            txtRemainingTime.text = "done!"
-        }
-    }
+	private var hasBenchmarkBeenStartedOnce = false
 
 
-    private suspend fun updateRemainingTime(remainingTimeSeconds: Int) {
-        for (time in remainingTimeSeconds downTo 0) {
-            if (time > 0) {
-                logThreadInfo("updateRemainingTime: $time seconds")
-                txtRemainingTime.text = "$time seconds remaining"
-                delay(1000)
-            } else {
-                txtRemainingTime.text = "done!"
-            }
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		benchmarkUseCase = compositionRoot.benchmarkUseCase
+	}
 
-    private fun logThreadInfo(message: String) {
-        ThreadInfoLogger.logThreadInfo(message)
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		val view = inflater.inflate(R.layout.fragment_loop_iterations_demo, container, false)
 
-    companion object {
-        fun newInstance(): Fragment {
-            return DesignDemoFragment()
-        }
-    }
+		txtRemainingTime = view.findViewById(R.id.txt_remaining_time)
+
+		btnStart = view.findViewById(R.id.btn_start)
+		btnStart.setOnClickListener {
+			logThreadInfo("button callback")
+
+			val benchmarkDurationSeconds = 5
+
+			coroutineScope.launch {
+				updateRemainingTime(benchmarkDurationSeconds)
+			}
+
+			coroutineScope.launch {
+				btnStart.isEnabled = false
+				val iterationsCount = benchmarkUseCase.executeBenchmark(benchmarkDurationSeconds)
+				Toast.makeText(requireContext(), "$iterationsCount", Toast.LENGTH_SHORT).show()
+				btnStart.isEnabled = true
+			}
+
+			hasBenchmarkBeenStartedOnce = true
+		}
+
+		return view
+	}
+
+	override fun onStop() {
+		logThreadInfo("onStop()")
+		super.onStop()
+		coroutineScope.coroutineContext.cancelChildren()
+		if (hasBenchmarkBeenStartedOnce) {
+			btnStart.isEnabled = true
+			txtRemainingTime.text = "done!"
+		}
+	}
+
+
+	private suspend fun updateRemainingTime(remainingTimeSeconds: Int) {
+		for (time in remainingTimeSeconds downTo 0) {
+			if (time > 0) {
+				logThreadInfo("updateRemainingTime: $time seconds")
+				txtRemainingTime.text = "$time seconds remaining"
+				delay(1000)
+			} else {
+				txtRemainingTime.text = "done!"
+			}
+		}
+	}
+
+	private fun logThreadInfo(message: String) {
+		ThreadInfoLogger.logThreadInfo(message)
+	}
+
+	companion object {
+		fun newInstance(): Fragment {
+			return DesignDemoFragment()
+		}
+	}
 }
