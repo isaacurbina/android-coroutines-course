3cf1db241806a807356796aea11391abe1a665fb
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-07 15:16:51 -0600

    47. Cooperative Cancellation (#24)

--- app/src/main/java/com/techyourchance/coroutines/demonstrations/coroutinescancellationcooperative/CancellableBenchmarkUseCase.kt
+++ app/src/main/java/com/techyourchance/coroutines/demonstrations/coroutinescancellationcooperative/CancellableBenchmarkUseCase.kt
@@ -1,6 +1,5 @@
 package com.techyourchance.coroutines.demonstrations.coroutinescancellationcooperative
 
-import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.common.ThreadInfoLogger.logThreadInfo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.isActive
@@ -8,19 +7,19 @@ import kotlinx.coroutines.withContext
 
 class CancellableBenchmarkUseCase {
 
-    suspend fun executeBenchmark(benchmarkDurationSeconds: Int) = withContext(Dispatchers.Default) {
-        logThreadInfo("benchmark started")
+	suspend fun executeBenchmark(benchmarkDurationSeconds: Int) = withContext(Dispatchers.Default) {
+		logThreadInfo("benchmark started")
 
-        val stopTimeNano = System.nanoTime() + benchmarkDurationSeconds * 1_000_000_000L
+		val stopTimeNano = System.nanoTime() + benchmarkDurationSeconds * 1_000_000_000L
 
-        var iterationsCount: Long = 0
-        while (System.nanoTime() < stopTimeNano && isActive) {
-            iterationsCount++
-        }
+		var iterationsCount: Long = 0
+		while (System.nanoTime() < stopTimeNano && isActive) {
+			iterationsCount++
+		}
 
-        logThreadInfo("benchmark completed")
+		logThreadInfo("benchmark completed")
 
-        iterationsCount
-    }
+		iterationsCount
+	}
 
 }
