9d6ca533eb25fd755caabdf0d7d348f740fbec74
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-07 15:31:59 -0600

    48. The Importance of Cancellation Exception (#25)

--- app/src/main/java/com/techyourchance/coroutines/demonstrations/coroutinescancellationcooperative2/CoroutinesCancellationCooperative2DemoFragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/demonstrations/coroutinescancellationcooperative2/CoroutinesCancellationCooperative2DemoFragment.kt
@@ -6,80 +6,89 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.techyourchance.coroutines.R
 import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger.logThreadInfo
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CoroutinesCancellationCooperative2DemoFragment : BaseFragment() {
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
+	private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
 
-    override val screenTitle get() = ScreenReachableFromHome.COROUTINES_CANCELLATION_COOPERATIVE_2_DEMO.description
+	override val screenTitle get() = ScreenReachableFromHome.COROUTINES_CANCELLATION_COOPERATIVE_2_DEMO.description
 
-    private lateinit var benchmarkUseCase: BlockingBenchmarkUseCase
+	private lateinit var benchmarkUseCase: BlockingBenchmarkUseCase
 
-    private lateinit var btnStart: Button
-    private lateinit var txtRemainingTime: TextView
+	private lateinit var btnStart: Button
+	private lateinit var txtRemainingTime: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        benchmarkUseCase = compositionRoot.blockingBenchmarkUseCase
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		benchmarkUseCase = compositionRoot.blockingBenchmarkUseCase
+	}
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_loop_iterations_demo, container, false)
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		val view = inflater.inflate(R.layout.fragment_loop_iterations_demo, container, false)
 
-        txtRemainingTime = view.findViewById(R.id.txt_remaining_time)
+		txtRemainingTime = view.findViewById(R.id.txt_remaining_time)
 
-        btnStart = view.findViewById(R.id.btn_start)
-        btnStart.setOnClickListener {
-            logThreadInfo("button callback")
+		btnStart = view.findViewById(R.id.btn_start)
+		btnStart.setOnClickListener {
+			logThreadInfo("button callback")
 
-            val benchmarkDurationSeconds = 5
+			val benchmarkDurationSeconds = 5
 
-            coroutineScope.launch() {
-                updateRemainingTime(benchmarkDurationSeconds)
-            }
+			coroutineScope.launch() {
+				updateRemainingTime(benchmarkDurationSeconds)
+			}
 
-            coroutineScope.launch(Dispatchers.Default) {
-                try {
-                    val iterationsCount = benchmarkUseCase.executeBenchmark(benchmarkDurationSeconds)
-                    logThreadInfo("benchmarked iterations: $iterationsCount")
-                } catch (e: CancellationException) {
-                    logThreadInfo("coroutine cancelled")
-                }
-            }
-        }
+			coroutineScope.launch(Dispatchers.Default) {
+				try {
+					val iterationsCount =
+						benchmarkUseCase.executeBenchmark(benchmarkDurationSeconds)
+					logThreadInfo("benchmarked iterations: $iterationsCount")
+				} catch (e: CancellationException) {
+					logThreadInfo("coroutine cancelled")
+				}
+			}
+		}
 
-        return view
-    }
+		return view
+	}
 
-    override fun onStop() {
-        logThreadInfo("onStop()")
-        super.onStop()
-        coroutineScope.coroutineContext.cancelChildren()
-    }
+	override fun onStop() {
+		logThreadInfo("onStop()")
+		super.onStop()
+		coroutineScope.coroutineContext.cancelChildren()
+	}
 
 
-    private suspend fun updateRemainingTime(remainingTimeSeconds: Int) {
-        for (time in remainingTimeSeconds downTo 0) {
-            if (time > 0) {
-                logThreadInfo("updateRemainingTime: $time seconds")
-                txtRemainingTime.text = "$time seconds remaining"
-                delay(1000)
-            } else {
-                txtRemainingTime.text = "done!"
-            }
-        }
-    }
+	private suspend fun updateRemainingTime(remainingTimeSeconds: Int) {
+		for (time in remainingTimeSeconds downTo 0) {
+			if (time > 0) {
+				logThreadInfo("updateRemainingTime: $time seconds")
+				txtRemainingTime.text = "$time seconds remaining"
+				delay(1000)
+			} else {
+				txtRemainingTime.text = "done!"
+			}
+		}
+	}
 
-    companion object {
-        fun newInstance(): Fragment {
-            return CoroutinesCancellationCooperative2DemoFragment()
-        }
-    }
+	companion object {
+		fun newInstance(): Fragment {
+			return CoroutinesCancellationCooperative2DemoFragment()
+		}
+	}
 }
