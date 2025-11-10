0f16dc54b55710e82aa5d629f032335cde3f6f8c
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-10 14:32:08 -0600

    50. Exercise 6 (#27)

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise6/Exercise6BenchmarkUseCase.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise6/Exercise6BenchmarkUseCase.kt
@@ -1,30 +1,28 @@
 package com.techyourchance.coroutines.exercises.exercise6
 
-import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.common.ThreadInfoLogger.logThreadInfo
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.withContext
 
 class Exercise6BenchmarkUseCase(private val postBenchmarkResultsEndpoint: PostBenchmarkResultsEndpoint) {
 
-    suspend fun executeBenchmark(benchmarkDurationSeconds: Int) = withContext(Dispatchers.Default) {
-        logThreadInfo("benchmark started")
+	suspend fun executeBenchmark(benchmarkDurationSeconds: Int) = withContext(Dispatchers.Default) {
+		logThreadInfo("benchmark started")
 
-        val stopTimeNano = System.nanoTime() + benchmarkDurationSeconds * 1_000_000_000L
+		val stopTimeNano = System.nanoTime() + benchmarkDurationSeconds * 1_000_000_000L
 
-        var iterationsCount: Long = 0
-        while (System.nanoTime() < stopTimeNano) {
-            iterationsCount++
-        }
+		var iterationsCount: Long = 0
+		while (System.nanoTime() < stopTimeNano) {
+			iterationsCount++
+		}
 
-        logThreadInfo("benchmark completed")
+		logThreadInfo("benchmark completed")
 
-        postBenchmarkResultsEndpoint.postBenchmarkResults(benchmarkDurationSeconds, iterationsCount)
+		postBenchmarkResultsEndpoint.postBenchmarkResults(benchmarkDurationSeconds, iterationsCount)
 
-        logThreadInfo("benchmark results posted to the server")
+		logThreadInfo("benchmark results posted to the server")
 
-        iterationsCount
-    }
+		iterationsCount
+	}
 
 }
