540761e483b1ed680e83d691ffc79c276ef08b53
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-10 14:49:18 -0600

    52. NonCancellable (#29)

--- app/src/main/java/com/techyourchance/coroutines/demonstrations/noncancellable/MakeCustomerPremiumUseCase.kt
+++ app/src/main/java/com/techyourchance/coroutines/demonstrations/noncancellable/MakeCustomerPremiumUseCase.kt
@@ -1,27 +1,25 @@
 package com.techyourchance.coroutines.demonstrations.noncancellable
 
-import com.techyourchance.coroutines.common.ThreadInfoLogger
-import com.techyourchance.coroutines.common.ThreadInfoLogger.logThreadInfo
-import com.techyourchance.coroutines.exercises.exercise6.PostBenchmarkResultsEndpoint
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.NonCancellable
+import kotlinx.coroutines.withContext
 
 class MakeCustomerPremiumUseCase(
-        private val premiumCustomersEndpoint: PremiumCustomersEndpoint,
-        private val customersDao: CustomersDao
+	private val premiumCustomersEndpoint: PremiumCustomersEndpoint,
+	private val customersDao: CustomersDao
 ) {
 
-    /**
-     * Give the customer premium status
-     * @return updated information about the customer
-     */
-    suspend fun makeCustomerPremium(customerId: String): Customer {
-        return withContext(Dispatchers.Default) {
-            withContext(NonCancellable) {
-                val updatedCustomer = premiumCustomersEndpoint.makeCustomerPremium(customerId)
-                customersDao.updateCustomer(updatedCustomer)
-                updatedCustomer
-            }
-        }
-    }
-
+	/**
+	 * Give the customer premium status
+	 * @return updated information about the customer
+	 */
+	suspend fun makeCustomerPremium(customerId: String): Customer {
+		return withContext(Dispatchers.Default) {
+			withContext(NonCancellable) {
+				val updatedCustomer = premiumCustomersEndpoint.makeCustomerPremium(customerId)
+				customersDao.updateCustomer(updatedCustomer)
+				updatedCustomer
+			}
+		}
+	}
 }
