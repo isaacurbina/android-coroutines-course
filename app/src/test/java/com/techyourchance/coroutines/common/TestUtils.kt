ddf7033fb62d8a2be034cf4bcd972a39cae4ce0b
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-11 16:42:57 -0600

    55. Coroutines Mechanics Part 1: CoroutineScope and CoroutineContext

--- app/src/test/java/com/techyourchance/coroutines/common/TestUtils.kt
+++ app/src/test/java/com/techyourchance/coroutines/common/TestUtils.kt
@@ -5,23 +5,23 @@ import kotlinx.coroutines.Job
 
 object TestUtils {
 
-    fun CoroutineScope.printCoroutineScopeInfo() {
-        println()
-        println("CoroutineScope: $this")
-        println("CoroutineContext: ${this.coroutineContext}")
-        println("Job: ${this.coroutineContext[Job]}")
-        println()
-    }
+	fun CoroutineScope.printCoroutineScopeInfo() {
+		println()
+		println("CoroutineScope: $this")
+		println("CoroutineContext: ${this.coroutineContext}")
+		println("Job: ${this.coroutineContext[Job]}")
+		println()
+	}
 
-    fun printJobsHierarchy(job: Job, nestLevel: Int = 0) {
-        val indent = "    ".repeat(nestLevel)
-        println("$indent- $job")
-        for (childJob in job.children) {
-            printJobsHierarchy(childJob, nestLevel + 1)
-        }
-        if (nestLevel == 0) {
-            println()
-        }
-    }
+	fun printJobsHierarchy(job: Job, nestLevel: Int = 0) {
+		val indent = "    ".repeat(nestLevel)
+		println("$indent- $job")
+		for (childJob in job.children) {
+			printJobsHierarchy(childJob, nestLevel + 1)
+		}
+		if (nestLevel == 0) {
+			println()
+		}
+	}
 
 }
