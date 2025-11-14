9c941762d9b6297dd7f6d3c441ed5d70558a15f3
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-14 16:33:27 -0600

    66. Exercise 8

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise8/FetchAndCacheUsersUseCase.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise8/FetchAndCacheUsersUseCase.kt
@@ -4,15 +4,14 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class FetchAndCacheUsersUseCase(
-        private val getUserEndpoint: GetUserEndpoint,
-        private val usersDao: UsersDao
+	private val getUserEndpoint: GetUserEndpoint,
+	private val usersDao: UsersDao
 ) {
 
-    suspend fun fetchAndCacheUsers(userIds: List<String>) = withContext(Dispatchers.Default) {
-        for (userId in userIds) {
-            val user = getUserEndpoint.getUser(userId)
-            usersDao.upsertUserInfo(user)
-        }
-    }
-
+	suspend fun fetchAndCacheUsers(userIds: List<String>) = withContext(Dispatchers.Default) {
+		for (userId in userIds) {
+			val user = getUserEndpoint.getUser(userId)
+			usersDao.upsertUserInfo(user)
+		}
+	}
 }
--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise8/UsersDao.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise8/UsersDao.kt
@@ -1,12 +1,15 @@
 package com.techyourchance.coroutines.exercises.exercise8
 
+import com.techyourchance.coroutines.common.ThreadInfoLogger.logThreadInfo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 
 class UsersDao {
 
-    suspend fun upsertUserInfo(user: User) = withContext(Dispatchers.IO) {
-        delay(500)
-    }
+	suspend fun upsertUserInfo(user: User) = withContext(Dispatchers.IO) {
+		logThreadInfo("upsertUserInfo($user)")
+		delay(1500)
+		logThreadInfo("upsertUserInfo -> done!)")
+	}
 }
