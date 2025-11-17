fccce38276135f5bcb4f538157a9e2435f603699
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-17 17:29:37 -0600

    68 Shared Mutable State

--- app/src/test/java/com/techyourchance/coroutines/demonstrations/incorrectparalleldecomposition/IncorrectParallelDecompositionDemoTest.kt
+++ app/src/test/java/com/techyourchance/coroutines/demonstrations/incorrectparalleldecomposition/IncorrectParallelDecompositionDemoTest.kt
@@ -1,42 +1,45 @@
 package com.techyourchance.coroutines.demonstrations.incorrectparalleldecomposition
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.math.pow
 
 class IncorrectParallelDecompositionDemoTest {
 
-    @Test
-    fun wrongUseOfCoroutines() = runBlocking {
-        var totalIterations = 0
-        withContext(Dispatchers.Default) {
-            for (duration in 1..5) {
-                launch {
-                    val startTimeNano = System.nanoTime()
-                    var iterations = 0
-                    while (System.nanoTime() < startTimeNano + (duration * 10f.pow(9))) {
-                        iterations++
-                    }
-                    totalIterations += iterations
-                }
-            }
-        }
-        println("total iterations: $totalIterations")
-    }
+	@Test
+	fun wrongUseOfCoroutines() = runBlocking {
+		var totalIterations = 0
+		withContext(Dispatchers.Default) {
+			for (duration in 1..5) {
+				launch {
+					val startTimeNano = System.nanoTime()
+					var iterations = 0
+					while (System.nanoTime() < startTimeNano + (duration * 10f.pow(9))) {
+						iterations++
+					}
+					totalIterations += iterations
+				}
+			}
+		}
+		println("total iterations: $totalIterations")
+	}
 
-    @Test
-    fun atomicityProblemDemo() = runBlocking {
-        var totalIterations = 0
-        withContext(Dispatchers.Default) {
-            repeat(100) {
-                launch {
-                    delay(100)
-                    totalIterations++
-                }
-            }
-        }
-        println("total iterations: $totalIterations")
-    }
-
-
+	@Test
+	fun atomicityProblemDemo() = runBlocking {
+		val totalIterations = AtomicInteger(0)
+		withContext(Dispatchers.Default) {
+			repeat(100) {
+				launch {
+					delay(100)
+					totalIterations.incrementAndGet()
+				}
+			}
+		}
+		println("total iterations: $totalIterations")
+	}
 }
