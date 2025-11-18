482fdd179b49f92b6b4e04eb3cdb12fd709aa096
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-17 18:19:16 -0600

    69. Async Coroutine Builder (#37)

--- app/src/test/java/com/techyourchance/coroutines/demonstrations/async/AsyncCoroutineBuilderDemoTest.kt
+++ app/src/test/java/com/techyourchance/coroutines/demonstrations/async/AsyncCoroutineBuilderDemoTest.kt
@@ -1,53 +1,58 @@
 package com.techyourchance.coroutines.demonstrations.async
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.junit.Test
 import kotlin.math.pow
 
 class AsyncCoroutineBuilderDemoTest {
 
-    @Test
-    fun correctUseOfCoroutinesVariant1() = runBlocking {
-        withContext(Dispatchers.Default) {
-            val deferreds = mutableListOf<Deferred<Int>>()
+	@Test
+	fun correctUseOfCoroutinesVariant1() = runBlocking {
+		withContext(Dispatchers.Default) {
+			val deferreds = mutableListOf<Deferred<Int>>()
 
-            for (duration in 1..5) {
-                deferreds.add(
-                        async {
-                            val startTimeNano = System.nanoTime()
-                            var iterations = 0
-                            while (System.nanoTime() < startTimeNano + (duration * 10f.pow(9))) {
-                                iterations++
-                            }
-                            iterations
-                        }
-                )
-            }
-            
-            var totalIterations = 0
-            for (deferred in deferreds) {
-                totalIterations += deferred.await()
-            }
+			for (duration in 1..5) {
+				deferreds.add(
+					async {
+						val startTimeNano = System.nanoTime()
+						var iterations = 0
+						while (System.nanoTime() < startTimeNano + (duration * 10f.pow(9))) {
+							iterations++
+						}
+						iterations
+					}
+				)
+			}
 
-            println("total iterations: $totalIterations")
-        }
-    }
+			var totalIterations = 0
+			for (deferred in deferreds) {
+				totalIterations += deferred.await()
+			}
 
-    @Test
-    fun correctUseOfCoroutinesVariant2() = runBlocking {
-        withContext(Dispatchers.Default) {
-            val totalIterations = (1..5).toList().map { duration ->
-                async {
-                    val startTimeNano = System.nanoTime()
-                    var iterations = 0
-                    while (System.nanoTime() < startTimeNano + (duration * 10f.pow(9))) {
-                        iterations++
-                    }
-                    iterations
-                }
-            }.awaitAll().sum()
+			println("total iterations: $totalIterations")
+		}
+	}
 
-            println("total iterations: $totalIterations")
-        }
-    }
+	@Test
+	fun correctUseOfCoroutinesVariant2() = runBlocking {
+		withContext(Dispatchers.Default) {
+			val totalIterations = (1..5).toList().map { duration ->
+				async {
+					val startTimeNano = System.nanoTime()
+					var iterations = 0
+					while (System.nanoTime() < startTimeNano + (duration * 10f.pow(9))) {
+						iterations++
+					}
+					iterations
+				}
+			}.awaitAll().sum()
+
+			println("total iterations: $totalIterations")
+		}
+	}
 }
