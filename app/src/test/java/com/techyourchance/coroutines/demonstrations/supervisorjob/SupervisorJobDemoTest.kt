f2c6746bc9fe36296e8fadf0f42efd489c81e84c
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-11-21 15:03:27 -0600

    77. SupervisorJob

--- app/src/test/java/com/techyourchance/coroutines/demonstrations/supervisorjob/SupervisorJobDemoTest.kt
+++ app/src/test/java/com/techyourchance/coroutines/demonstrations/supervisorjob/SupervisorJobDemoTest.kt
@@ -1,79 +1,84 @@
 package com.techyourchance.coroutines.demonstrations.supervisorjob
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
-import java.lang.RuntimeException
 
 class SupervisorJobDemoTest {
 
-    @Test
-    fun uncaughtExceptionInConcurrentCoroutines() {
-        runBlocking {
-            val scopeJob = Job()
-            val scope = CoroutineScope(scopeJob + Dispatchers.Default)
-            val job1 = scope.launch {
-                delay(100)
-                println("inside coroutine")
-            }
-            val job2 = scope.launch {
-                delay(50)
-                throw RuntimeException()
-            }
-            joinAll(job1, job2)
-            println("scopeJob: $scopeJob")
-            println("job1: $job1")
-            println("job2: $job2")
-        }
-        Thread.sleep(100)
-        println("test completed")
-    }
+	@Test
+	fun uncaughtExceptionInConcurrentCoroutines() {
+		runBlocking {
+			val scopeJob = Job()
+			val scope = CoroutineScope(scopeJob + Dispatchers.Default)
+			val job1 = scope.launch {
+				delay(100)
+				println("inside coroutine")
+			}
+			val job2 = scope.launch {
+				delay(50)
+				throw RuntimeException()
+			}
+			joinAll(job1, job2)
+			println("scopeJob: $scopeJob")
+			println("job1: $job1")
+			println("job2: $job2")
+		}
+		Thread.sleep(100)
+		println("test completed")
+	}
 
-    @Test
-    fun uncaughtExceptionInConcurrentCoroutinesWithSupervisorJob() {
-        runBlocking {
-            val scopeJob = SupervisorJob()
-            val scope = CoroutineScope(scopeJob + Dispatchers.Default)
-            val job1 = scope.launch {
-                delay(100)
-                println("inside coroutine")
-            }
-            val job2 = scope.launch {
-                delay(50)
-                throw RuntimeException()
-            }
-            joinAll(job1, job2)
-            println("scopeJob: $scopeJob")
-            println("job1: $job1")
-            println("job2: $job2")
-        }
-        Thread.sleep(100)
-        println("test completed")
-    }
+	@Test
+	fun uncaughtExceptionInConcurrentCoroutinesWithSupervisorJob() {
+		runBlocking {
+			val scopeJob = SupervisorJob()
+			val scope = CoroutineScope(scopeJob + Dispatchers.Default)
+			val job1 = scope.launch {
+				delay(100)
+				println("inside coroutine")
+			}
+			val job2 = scope.launch {
+				delay(50)
+				throw RuntimeException()
+			}
+			joinAll(job1, job2)
+			println("scopeJob: $scopeJob")
+			println("job1: $job1")
+			println("job2: $job2")
+		}
+		Thread.sleep(100)
+		println("test completed")
+	}
 
-    @Test
-    fun uncaughtExceptionInConcurrentCoroutinesWithSupervisorJobAndExceptionHandler() {
-        runBlocking {
-            val coroutineExceptionHandler = CoroutineExceptionHandler { _, _ ->
-                println("Caught exception")
-            }
-            val scopeJob = SupervisorJob()
-            val scope = CoroutineScope(scopeJob + Dispatchers.Default + coroutineExceptionHandler)
-            val job1 = scope.launch {
-                delay(100)
-                println("inside coroutine")
-            }
-            val job2 = scope.launch {
-                delay(50)
-                throw RuntimeException()
-            }
-            joinAll(job1, job2)
-            println("scopeJob: $scopeJob")
-            println("job1: $job1")
-            println("job2: $job2")
-        }
-        Thread.sleep(100)
-        println("test completed")
-    }
-
-
+	@Test
+	fun uncaughtExceptionInConcurrentCoroutinesWithSupervisorJobAndExceptionHandler() {
+		runBlocking {
+			val coroutineExceptionHandler = CoroutineExceptionHandler { _, _ ->
+				println("Caught exception")
+			}
+			val scopeJob = SupervisorJob()
+			val scope = CoroutineScope(scopeJob + Dispatchers.Default + coroutineExceptionHandler)
+			val job1 = scope.launch {
+				delay(100)
+				println("inside coroutine")
+			}
+			val job2 = scope.launch {
+				delay(50)
+				throw RuntimeException()
+			}
+			joinAll(job1, job2)
+			println("scopeJob: $scopeJob")
+			println("job1: $job1")
+			println("job2: $job2")
+		}
+		Thread.sleep(100)
+		println("test completed")
+	}
 }
